dab79f028f01babcb4b5b087862ab9bcc8f590c0
Emilia Hansson <eh222yn@student.lnu.se>
2022-01-03 21:41:55 +0100

    Add smileys object for translating smileys in text

--- src/js/components/my-chat/my-chat.js
+++ src/js/components/my-chat/my-chat.js
@@ -107,6 +107,17 @@ customElements.define('my-chat',
     #nicknameForm
     #socket
     #chatOutput
+    #emojis = {
+      ':)': '😊',
+      ':D': '😃',
+      xD: '😆',
+      XD: '😂',
+      ':(': '😞',
+      ':@': '😡',
+      ':\'(': '😢',
+      ':=(': '😭'
+    }
+
     /**
      * Creates an instance of current type.
      */
@@ -185,7 +196,20 @@ customElements.define('my-chat',
       const data = JSON.parse(event.data)
       if (data.type === 'notification' || data.type === 'message') {
         const message = document.createElement('p')
-        message.textContent = `${data.username}: ${data.data}`
+        let messageData = data.data
+        const emojis = Object.keys(this.#emojis)
+        const emojiCodes = Object.values(this.#emojis)
+        for (let i = 0; i < emojis.length; i++) {
+          const emoji = emojis[i]
+          for (let j = 0; j < messageData.length - 1; j++) {
+            if (emoji[0] === messageData[j]) {
+              if (emoji[1] === messageData[j + 1]) {
+                messageData = messageData.replace(messageData[j] + messageData[j + 1], emojiCodes[i])
+              }
+            }
+          }
+        }
+        message.textContent = `${data.username}: ${messageData}`
         data.channel === 'emilias-channel' ? message.setAttribute('right', '') : message.setAttribute('left', '')
         this.#chatOutput.append(message)
       }
